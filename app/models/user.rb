cec0b9d76c3bb5be5b3fa76be53806b6c6f11748
sugiyamam <ms57926@gmail.com>
2021-01-26 14:53:27 +0900

    ユーザー管理機能実装提出3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 40 }
-  validates :email, presence: true
-  validates :password, presence: true, format: { with: /\A[a-z\d]{6,100}+\z/i, message: "is invalid. Input full-width katakana characters." }
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_family_name, presence: true
-  validates :kana_first_name, presence: true
+  validates :password, presence: true, format: { with: /\A[a-z\d]{6,100}+\z/i, message: "is invalid. Password Include both letters and numbers" }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width kanji hiragana katakana characters" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width kanji hiragana katakana characters" }
+  validates :kana_family_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :date_of_birth, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,90 +7,111 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-    # 1
-    it 'nickname:必須' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+  describe 'ユーザー管理機能'
+    context 'うまくいく場合' do
+      it 'フォームを入力すれば登録可能' do
+          expect(@user).to be_valid
+      end
     end
-
-    # 2
-    it 'email:必須' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-
-    #3
-    it '重複したemailが存在する場合は登録不可' do
-      user = FactoryBot.create(:user)
-      another_user = FactoryBot.build(:user, email: user.email)
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include("Email has already been taken")
-    end
-
-    # 4
-    it 'password:半角英数混合(半角英語のみ)' do
-      @user.password = 'あああAAA'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-
-    # 5
-    it "passwordが存在してもpassword_confirmationがない場合は登録不可" do
-      @user.password = 'あああAAA'
-      @user.password_confirmation = 'abc000'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password Include both letters and numbers")
-    end
-
-    # 6
-    it "passwordが6文字以上であれば登録可能" do
-      @user.password = 'aaaaaa'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    end
-
-    # 7
-    it "passwordが5文字以下であれば登録不可" do
-      @user.password = 'aaaaa'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    end
-
-    # 8
-    it "family_nameがない場合は登録不可" do
-      @user.family_name = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Family name can't be blank")
-    end
-
-    # 9
-    it "first_nameがない場合は登録不可" do
-      @user.first_name = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("First name can't be blank")
-    end
-
-    # 10
-    it "kana_family_nameがない場合は登録不可" do
-      @user.kana_family_name = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Kana family name can't be blank")
-    end
-
-    # 11
-    it "kana_nameがない場合は登録不可" do
-      @user.kana_first_name = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Kana first name can't be blank")
-    end
-
-    # 12
-    it "date_of_birthがない場合は登録不可" do
-      @user.date_of_birth = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Date of birth can't be blank")
+    
+    context 'うまくいかない場合' do
+      it 'nickname:未入力' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'email:未入力' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'email:@未入力' do
+        @user.email = 'ああAa11'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it '重複したemailが存在する場合は登録不可' do
+        user = FactoryBot.create(:user)
+        another_user = FactoryBot.build(:user, email: user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'password:未入力' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'password:英語のみで入力不可' do
+        @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'password:数字のみで入力不可' do
+        @user.password = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'password:全角のみで入力不可' do
+        @user.password = 'あああAAA'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it "passwordが存在してもpassword_confirmationがない場合は登録不可" do
+        @user.password = 'あああAAA'
+        @user.password_confirmation = 'abc000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it "passwordが5文字以下であれば登録不可" do
+        @user.password = 'aaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'family_name:未入力' do
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
+      end
+      it 'family_name:漢字ひらがなカタカナ以外入力不可' do
+        @user.family_name = 'Aa00'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name is invalid. Input full-width kanji hiragana katakana characters")
+      end
+      it 'first_name:未入力' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it "first_name:漢字ひらがなカタカナ以外入力不可" do
+        @user.family_name = 'Aa00'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name is invalid. Input full-width kanji hiragana katakana characters")
+      end
+      it 'kana_family_name:未入力' do
+        @user.kana_family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Kana family name can't be blank")
+      end
+      it 'kana_family_name:全角カタカナ以外入力不可' do
+        @user.kana_family_name = 'Aa00'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Kana family name is invalid. Input full-width katakana characters")
+      end
+      it 'kana_name:未入力' do
+        @user.kana_family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Kana family name can't be blank")
+      end
+      it 'kana_name:全角カタカナ以外入力不可' do
+        @user.kana_family_name = 'あAa0'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Kana family name is invalid. Input full-width katakana characters")
+      end
+      it "date_of_birthがない場合は登録不可" do
+        @user.date_of_birth = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Date of birth can't be blank")
+      end
     end
   end
 end
